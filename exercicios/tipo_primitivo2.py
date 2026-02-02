c8af6046a3683e1c407bd8575e3078350e9ab287
Antônio José <t985420674@gmail.com>
2026-02-02 18:18:34 -0300

    tipo primitivo

---
+++ exercicios/tipo_primitivo2.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um número: ")) # A importancia de índentificação da variável
+n2 = int(input("Digite outro: "))
+s = n1 + n2
+print(" A soma vale", s) 
+print(type(s))
+
+
+# A importancia de índentificação da variável
--- exercicios/tipos primitivos.py
+++ exercicios/tipos primitivos.py
@@ -1,4 +1,4 @@
-n1 = input("Digite um número: ")
+n1 = input("Digite um número: ") # A importancia de índentificação da variável
 n2 = input("Digite outro: ")
 s = n1 + n2
 print(" A soma vale", s) 
