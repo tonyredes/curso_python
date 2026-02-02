d981fef15c0c8d73657e59767eb84840478aa82e
Antônio José <t985420674@gmail.com>
2026-02-02 18:34:02 -0300

    .format

---
+++ exercicios/tipo_primitivo3.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um número: ")) # A importancia de índentificação da variável
+n2 = int(input("Digite outro: "))
+s = n1 + n2
+print(" A soma entre {} e {} é igual a {}".format(n1, n2, s)) 
+print(type(s))
+
+
+# A importancia de índentificação da variável
+# Uma forma de formatação de texto mais moderna (f-strings)
